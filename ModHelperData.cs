c1bc43780b387962cadb9e528ce18488d97f9749
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2023-09-01 15:33:32 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,4 +7,5 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "TheLongestRoad";
+    public const string WorksOnVersion = "38.3";
 }
